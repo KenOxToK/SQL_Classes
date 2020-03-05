2b0774eafe79fcf34abfd9f341686535d6ee4572
Thiago Kenj <61833052+kenqdk@users.noreply.github.com>
2020-03-05 12:53:25 -0300

    Update cursos.sql

--- cursos.sql
+++ cursos.sql
@@ -1,11 +1,7 @@
- 
-
-
-
-CREATE TABLE cursos (
+ CREATE TABLE cursos (
  id_curso INT auto_increment primary key,
  nome varchar(50) NOT NULL,
  duracao INT NOT NULL,
  periodo VARCHAR (30) NUll);
  
- SELECT * FROM jogo.tbjogadores 
+ SELECT * FROM curso 
