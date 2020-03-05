60e1f4cfc3093bb6902a97877565aa7dbb5ff59b
kenqdk <61833052+kenqdk@users.noreply.github.com>
2020-03-05 10:58:25 -0300

    information curso

---
+++ cursos.sql
@@ -0,0 +1,11 @@
+ 
+
+
+
+CREATE TABLE cursos (
+ id_curso INT auto_increment primary key,
+ nome varchar(50) NOT NULL,
+ duracao INT NOT NULL,
+ periodo VARCHAR (30) NUll);
+ 
+ SELECT * FROM jogo.tbjogadores 
